a8262a03553c153c95ee9c0ca665bfd8c950b9bf
MityaSorokin <55582179+MityaSorokin@users.noreply.github.com>
2023-01-13 17:34:30 +0100

    Upload test_items.py and conftest.py

---
+++ test_items.py
@@ -0,0 +1,20 @@
+import pytest
+import time
+from selenium.webdriver.common.by import By
+
+links = [
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+]
+
+
+@pytest.mark.parametrize('link', links)
+def test_find_add_to_basket_button(browser, link):
+    browser.get(link)
+    curr_language = browser.execute_script('return window.navigator.language || window.navigator.userLanguage')
+    tim.sleep(3)
+    if 'fr' in curr_language:
+        time.sleep(30)
+    find_buttons = browser.find_elements(By.CSS_SELECTOR, '#add_to_basket_form .btn-add-to-basket')
+    assert len(find_buttons) > 0, 'Не найдена кнопка добавления в корзину'
+    assert len(find_buttons) < 2, 'Кнопка добавления в корзину не уникальна'
